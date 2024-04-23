60c0788b74617579b84a807b319bbc64bf8d3675
coderomar1 <omarabofahad0@gmail.com>
2024-04-24 01:21:04 +0300

    new branch db

--- qustions.js
+++ qustions.js
@@ -1,6 +1,6 @@
 
 class Questions{
-    constructor(q,c1,c2,c3,c4){
+    constructor(q){
         this.q = q;
     }
 }
@@ -20,35 +20,59 @@ class TrueAndFalse extends Questions {
         this.c = c;
     }
 }
+class Selection extends Questions {
+    constructor(q,c1,c2,c3,c4){
+        super(q)
+        this.c1 = c1;
+        this.c2 = c2;
+        this.c3 = c3;
+        this.c4 = c4;
+    }
+}
 
 const arr = [
-    new Choices("A storage with no moving parts " /*<- question */,"Magnetic storage","Optical storage","Digital versatile disc","Solid-state storage"),
-    new Choices("The multimedia effect includes the following " /*<- question */," Identification, Categorization, Preservation","Input, processing, storing, output","None of the choices","compatibility, creation, distribution"),
-    new Choices("Effective file maintenance involves:" /*<- question */," Input, processing, storing, output"," compatibility, creation, distribution"," None of the choices","Identification, Categorization, Preservation"),
-    new Choices(". A container for binary codes" /*<- question */," File size"," Program files"," File extensions"," Digital files"),
-    new Choices(" The type of files that contains executable instructions" /*<- question */," File size"," File compatibility"," File extensions"," Program files"),
-    new Choices(" Process of re-encoding digital data to reduce file size is called:" /*<- question */," Sample resolution"," Sample rate"," None of the choices listed"," File compression"),
-    new Choices("An encoding that displays the full range a human eye can perceive" /*<- question */," 16-bit sound"," ASCII-8"," Unicode"," 24-bit color"),
-    new Choices(" ASCII-8 (8 bit code) can represent"," Over 65,000 different characters" /*<- question */," Plays the full decibel range the human ear can perceive"," 128 letters, numbers, & symbols in English language"," 256 letters, numbers, & symbols in English language"),
-    new Choices("Number of bits used to represent digital sample is called:" /*<- question */," Sample Rate"," Sample formats"," Sample sound"," Sample Resolution"),
-    new Choices("It is data made useful, interpreted, and applied to produce understanding"/*<- question */," Symbols"," Data "," Digital files"," Information"),
-    new Choices(" A type of storage that holds data and instructions outside the system unit for long periods of time" /*<- question */," Hard drive storage"," Optical storage"," Magnetic storage"," Secondary storage"),
-    new TrueAndFalse(" Series of letters to designate the file format"/*<- question */,true),
-    new TrueAndFalse(" Ability to use the file in a different platform of hardware and software"/*<- question */,true),
-    new TrueAndFalse("Information is the givens of experience such as, measurements, facts, observations"/*<- question */,false),
-    new TrueAndFalse("Digital data consists of discrete and separate units " /*<- question */,true),
-    new TrueAndFalse("Lossy is the efficient encoding reduces file size without loss of original data " /*<- question */,false),
-    new TrueAndFalse("Adding 2 to the power doubles the number of distinct data items that can be encoded"/*<- question */,false),
-    new TrueAndFalse("In non-linear multimedia, the user has no control over the flow of information"/*<- question */,false),
-    new TrueAndFalse("Interactive multimedia is non-linear which means user is a participant in the flow of information"/*<- question */,true),
-    new TrueAndFalse("Analog data is characterized by a function that varies continuously"/*<- question */,true),
-    new TrueAndFalse(" Digital encoding assigns bits to data items"/*<- question */,true),
-    new TrueAndFalse("Codec is a program to compress a file into a smaller size and decompress it into a usable form"/*<- question */,true),
-    new TrueAndFalse("Contemporary Multimedia is the development, integration, and delivery of any combination of text, graphics, animation, sound or video through a computer or other electronic means"/*<- question */,true),
-    new TrueAndFalse("the best compression strategy best suited for an important database of student names and addresses is lossy",true),
-    new TrueAndFalse("Interactive multimedia enhances learning, memory & retention by 15 percent",false),
-    new TrueAndFalse("Laser printer is an example of impact printers",false),
-    new TrueAndFalse(`Ports that are powered through the interface port and that means no more "wall warts"`,true),
+ // Mid
+  new TrueAndFalse("Data can be processed"/* quirstion*/,true),
+  new TrueAndFalse("you are a student. it is example about an information"/* quirstion*/,true),
+  new TrueAndFalse("We can make a decision based on some information"/* quirstion*/,true),
+  new TrueAndFalse("We can make a decision based on enough data"/* quirstion*/,false),
+  new TrueAndFalse("database can be not organized"/* quirstion*/,false),
+  new TrueAndFalse("Some types of data, such as images, cannot be stored in database"/* quirstion*/,false),
+  new Choices("to make ER model in a good shape and avoid any redundancy of data we have to normalize it based on "/* quirstion*/," 2NF"," Create primary and Foreign key "," Relationship"," 3NF"),
+  new Choices(" an interface between the database and its programs as known "/* quirstion*/," database"," Relationship"," Database Administrator"," database management system "),
+  new Choices(" determine the requirements of the database users"/* quirstion*/," Application programmer"," Database user"," Business rule"," System analyst"),
+  new Choices(" anything about which data will be collected and stored"/* quirstion*/," An attribute"," A relationship"," A constraint"," An entity"),
+  new Choices(" It will be considered as a column in the database"/* quirstion*/," An entity"," A relationship"," A constraint"," An attribute"),
+  new Choices(" To solve Many-to-many relationship between entities, we come to use "/* quirstion*/," nothing"," create new attribute "," remove one entity"," associative entity"),
+  new Choices(" Each table row represents"/* quirstion*/," two-dimensional"," single value "," same domain "," single entity "),
+  new Selection(" Each entity must have at least one (select 2)"/* quirstion*/,"Foreign key ","ID ","Primary key ","Not null value "),
+  //Quiz 1
+  new TrueAndFalse("Omar is manager; it can be considered as example of information"/* quirstion*/,true),
+  new TrueAndFalse("we can call Relational databases as NoSQL."/* quirstion*/,false),
+  new TrueAndFalse("Information can be defined as a set of isolated."/* quirstion*/,false),
+  new TrueAndFalse("Data is more strict."/* quirstion*/,false),
+  new TrueAndFalse("Data can be processed."/* quirstion*/,true),
+  new TrueAndFalse("Data is raw fact."/* quirstion*/,true),
+  new TrueAndFalse("in some cases, we can call data as information."/* quirstion*/,false),
+  new TrueAndFalse("from data we can make a decision."/* quirstion*/,false),
+  // Quiz 2
+  new Choices("A column-level constraint references:","	primary key","a multiple columns","table","a single column"),
+  new Choices("choose the correct sentence:","each column must have one primary key","A table must have foreign key","A table should not have any constraint","A table must have at least one primary key"),
+  new TrueAndFalse("When defining PRIMARY KEY columns, we should write name of table with constraint name as prefix.",false),
+  new Choices("part of scenario to create a ER diagram , A dealer has many different cars how many entities are.","	three entities","no entity","one entity","two entities"),
+  new Choices("where we can add group function (ex , sum ).","where clause","where or select clauses","select clause","having or select clauses only"),
+  new Choices("College may have several school. each school belongs to one and only one college , the correct realtion between these entities will be:","(0,M) :(0,M)","(0,M): (1,0)","(0,1) : (0,M)","(1,1) : (0,M)"),
+  new Choices("number data type uses to:","for first column","Not all of the above","ID","for column that contains only number value"),
+ /*updated */new Choices("select the wrong sentences:","Primary key constraints can be defined at the table level","Primary key constraints can be defined at the column level","Primary key constraints can be added after create table","Primary key constraints can contain one null value"),
+  new Choices("when we want to build new table , we use:","constraint","use Alter table","Add Table","Create"),
+  new Choices("PRIMARY KEY should include:","primary-key value can appear in more than one row in the table","Not all of the above","primary key may have one null value","A primary key column cannot contain duplicate values"),
+  new Choices("we can remove a column name by using the following statement:","We can not remove a column after created it","Alter column column1 drop column Loc","Alter Table TableName Delete Column ColumnName","Alter table table1 drop column Loc ;"),
+  new Choices("The advantage of the CONCAT function is:","change a small letter to a capital letter","do nothing","A and B","similar to the concatenation operator"),
+  new TrueAndFalse("We can have a column in a table and it can also appear as a column in another table:",true),
+  new Choices("What is the correct order:","CONSTRAINT , clients_client_num_pk , PRIMARY KEY","CONSTRAINT1 clients_client_num_pk PRIMARY KEY","CONSTRAINT1 PRIMARY KEY , CONSTRAINT","CONSTRAINT CL2_CL1_PK PRIMARY KEY"),
+  new Selection("in associative entity , the primary key will come from:","it has no primary key","Not all of the above","the primary keys of the two fundamental entities ( parent entities)","It has a new attribute as primary key"),
+  new Choices("when We want to define the data type of the column as a string, We will use:","string","number","varchar","varchar2"),
+  new Choices("we can change a column name by using the following statement:","Alter table emp update ( name1 varchar2(20) ) ;","Alter table emp update ( name1 VARCHAR2(10) ) ;","update column emp modify ( ID numner(10,2) ) ;","Alter table emp modify ( name1 numner(8,1) )"),
 ];
 
 
